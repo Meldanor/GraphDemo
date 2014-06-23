c713eb04d5acc9f3e6212d73af4faff3ebb166cc
Kilian Gärtner <Bhaals@gmx.de>
2014-06-23 20:16:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GraphDemo
+
+Simple project to demonstrate the importance of using the correct datastructure for the A-Star algorithm.
---
+++ src/main/java/de/meldanor/graphdemo/Core.java
@@ -0,0 +1,8 @@
+package de.meldanor.graphdemo;
+
+public class Core {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is Graph Demo by Meldanor!");
+    }
+}
